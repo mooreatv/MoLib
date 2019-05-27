eb18ed6a26cbe52f6e70cee9330290a596be0c64
Moorea Tv <moorea@ymail.com>
2019-05-26 17:51:12 -0700

    adding pkgmeta.yaml
    to help keeping file/release clean

--- installwt.bat
+++ installwt.bat
@@ -1,4 +1,4 @@
 set wowdir=C:\Program Files (x86)\World of Warcraft
 set addonsdir=%wowdir%\_retail_\Interface\Addons
 set tdir=%addonsdir%\WhoTracker
-xcopy /i /y *.* "%tdir%"
+xcopy /i /y WhoTracker.* ChangeLog.txt "%tdir%"
