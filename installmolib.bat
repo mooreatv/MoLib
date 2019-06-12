789e010b428f6a316452a5e661a4d81b42c39308
Moorea Tv <moorea@ymail.com>
2019-06-12 09:52:04 -0700

    adding _ptr_ to list of installs

--- installmolib.bat
+++ installmolib.bat
@@ -1,4 +1,4 @@
-for %%i in (_retail_ _classic_beta_) do (
+for %%i in (_retail_ _classic_beta_ _ptr_) do (
     for %%a in (WhoTracker DynamicBoxer Camera) do (
         echo Installing for %%i and %%a
         xcopy /i /y *.lua "C:\Program Files (x86)\World of Warcraft\%%i\Interface\Addons\%%a\MoLib\"
