a119e6f96610a44b9e4bcc17ce691a3bc302bb26
Moorea Tv <moorea@ymail.com>
2019-06-28 01:24:08 -0700

    also install molib locally to RandomGenerator

--- installmolib.bat
+++ installmolib.bat
@@ -1,5 +1,5 @@
 for %%i in (_retail_ _classic_ _classic_beta_ _ptr_) do (
-    for %%a in (WhoTracker DynamicBoxer Camera) do (
+    for %%a in (WhoTracker DynamicBoxer Camera RandomGenerator) do (
         echo Installing for %%i and %%a
         xcopy /i /y *.lua "C:\Program Files (x86)\World of Warcraft\%%i\Interface\Addons\%%a\MoLib\"
     )
