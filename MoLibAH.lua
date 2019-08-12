7789fd78863fe3d19ff31108ccaf2b5f109992ab
Moorea Tv <moorea@ymail.com>
2019-08-12 01:06:35 -0700

    initial version of MoLibAH

---
+++ MoLibAH.lua
@@ -0,0 +1,47 @@
+--[[
+  MoLib (Auction House part) -- (c) 2019 moorea@ymail.com (MooreaTv)
+  Covered by the GNU General Public License version 3 (GPLv3)
+  NO WARRANTY
+  (contact the author if you need a different license)
+]] --
+-- our name, our empty default (and unused) anonymous ns
+local addonName, _ns = ...
+
+local ML = _G[addonName]
+
+function ML:AHSaveAll()
+  local _, dumpOk = CanSendAuctionQuery()
+  if not dumpOk then
+    self:Warning("Can't query ALL at AH, try again later...")
+    return
+  end
+  -- SetAuctionsTabShowing(false)
+  QueryAuctionItems("", nil, nil, 0, 0, 0, true)
+  self:AHdump()
+end
+
+function ML:AHdump()
+  local batch, count = GetNumAuctionItems("list")
+  if batch ~= count then
+    self:Error("Unexpected mismatch between batch % and count % for a dump all of AH", batch, count)
+    -- SetAuctionsTabShowing(true)
+    return
+  else
+    if count == 0 then
+      self:PrintDefault("Result not ready, try :AHdump() shortly")
+      return
+    end
+    self:PrintInfo("Got % items from AH all dump.", count)
+  end
+  local res = {}
+  for i = 1, count do
+    table.insert(res, {info = {GetAuctionItemInfo("list", i)}, link = GetAuctionItemLink("list", i)})
+  end
+  -- SetAuctionsTabShowing(true)
+  if not self.savedVar.ah then
+    self.savedVar.ah = {}
+  end
+  local p, r = UnitFullName("player")
+  table.insert(self.savedVar.ah, {realm=r, ts = GetServerTime(), data = res, char=p})
+  return res
+end
